8f4db9774b9aa52498d8d2545ed5a4f10131d7ad
VISHNU <Vishnuvishva2434@gmail.com>
2023-09-17 13:16:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
-#(©)Codexbotz
-
+#(©)𝗠𝗿_𝗩_𝗯𝗼𝘁𝘀
 from aiohttp import web
 from plugins import web_server
 
@@ -12,7 +11,7 @@ from datetime import datetime
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
 
 
-name =""" ❤️ WebX Bots """
+name =""" 𝗠𝗿_𝗩_𝗯𝗼𝘁𝘀 """
 
 
 class Bot(Client):
@@ -45,7 +44,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Web_X_bots for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Mr_V_bots for support")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -55,12 +54,12 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Web_X_Bots for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Mr_V_bots for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
-        self.LOGGER(__name__).info(f""" \n\n   ❤️ WebXBots """)
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Mr_V_bots")
+        self.LOGGER(__name__).info(f""" \n\n  𝗠𝗿_𝗩_𝗯𝗼𝘁𝘀 """)
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
@@ -70,4 +69,4 @@ class Bot(Client):
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
+        self.LOGGER(__name__).info("ooh no there is an error, so Bot stopped.")
